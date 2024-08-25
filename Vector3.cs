d43403abf39760d9fdd4332f977281b6bf06a515
my-keki <my.keki@outlook.com>
2024-08-24 19:52:56 -0700

    Implemented custom 3d vector class

---
+++ Vector3.cs
@@ -0,0 +1,83 @@
+namespace RayTracer;
+
+class Vector3
+{
+    double X { get; set; }
+    double Y { get; set; }
+    double Z { get; set; }
+
+    public Vector3() {
+        X = 0;
+        Y = 0;
+        Z = 0;
+    }
+
+    public Vector3(double x, double y, double z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+
+    public static Vector3 operator - (Vector3 v)
+    {
+        return new Vector3(-v.X, -v.X, -v.X);
+    }
+
+    public static Vector3 operator + (Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
+    }
+
+    public static Vector3 operator - (Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
+    }
+
+    public static Vector3 operator * (Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.X * v2.X, v1.Y * v2.Y, v1.Z * v2.Z);
+    }
+
+    public static Vector3 operator * (Vector3 v, double t)
+    {
+        return new Vector3(v.X * t, v.Y * t, v.Z * t);
+    }
+
+    public static Vector3 operator / (Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.X / v2.X, v1.Y / v2.Y, v1.Z / v2.Z);
+    }
+
+    public static Vector3 operator / (Vector3 v, double t)
+    {
+        return new Vector3(v.X / t, v.Y / t, v.Z / t);
+    }
+
+    public double Length()
+    {   
+        return Math.Sqrt(X * X + Y * Y + Z * Z);
+    }
+
+    public double LengthSquared()
+    {
+        return X * X + Y * Y + Z * Z;
+    }
+
+    public static Vector3 DotProduct(Vector3 v1, Vector3 v2)
+    {
+        return new Vector3(v1.Y * v2.Z - v1.Z * v2.Y, v1.Z * v2.Z - v1.Z * v2.Z, v1.X * v2.Y - v1.Y * v2.X);
+    }
+
+    public static Vector3 UnitVector(Vector3 v)
+    {
+        return v / v.Length();
+    }
+
+    public override string ToString()
+    {
+        return $"{X}, {Y}, {Z}";
+    }
+
+
+}
